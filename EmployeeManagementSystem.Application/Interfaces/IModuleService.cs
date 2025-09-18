92bf95ece3dbee4748c4453b746e44ed7c21eaf2
poshan-p <poshanpeeroo@gmail.com>
2025-09-18 21:08:47 +0400

    feat(modules): implement module management system

---
+++ EmployeeManagementSystem.Application/Interfaces/IModuleService.cs
@@ -0,0 +1,9 @@
+using EmployeeManagementSystem.Application.DTOs;
+
+namespace EmployeeManagementSystem.Application.Interfaces
+{
+    public interface IModuleService
+    {
+        Task<ApiResponse<ModulesResponseDto>> GetModulesAsync(string userRoleRefCode);
+    }
+}
